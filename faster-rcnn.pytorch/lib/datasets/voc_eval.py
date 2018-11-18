ddb11ddb8b73ddb6d38d16907633655516d923ce
immuno121 <shasvat.desai@gmail.com>
2018-11-18 16:27:51 -0500

    corrected annotations

--- faster-rcnn.pytorch/lib/datasets/voc_eval.py
+++ faster-rcnn.pytorch/lib/datasets/voc_eval.py
@@ -115,6 +115,7 @@ def voc_eval(detpath,
   if not os.path.isdir(cachedir):
     os.mkdir(cachedir)
   cachefile = os.path.join(cachedir, '%s_annots.pkl' % imagesetfile)
+  print(cachefile)
   # read list of images
   with open(imagesetfile, 'r') as f:
     lines = f.readlines()
