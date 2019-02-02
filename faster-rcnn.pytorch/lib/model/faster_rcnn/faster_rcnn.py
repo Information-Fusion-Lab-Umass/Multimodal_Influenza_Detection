6be69ac7c9d23ff6b404f469d454a663060eeea2
immuno121 <shasvat.desai@gmail.com>
2019-02-02 18:17:22 -0500

    tryng to debug

--- faster-rcnn.pytorch/lib/model/faster_rcnn/faster_rcnn.py
+++ faster-rcnn.pytorch/lib/model/faster_rcnn/faster_rcnn.py
@@ -106,6 +106,11 @@ class _fasterRCNN(nn.Module):
 
             # bounding box regression L1 loss
             RCNN_loss_bbox = _smooth_l1_loss(bbox_pred, rois_target, rois_inside_ws, rois_outside_ws)
+	    rpn_loss_cls = torch.unsqueeze(rpn_loss_cls, 0)
+            rpn_loss_bbox = torch.unsqueeze(rpn_loss_bbox, 0)
+            RCNN_loss_cls = torch.unsqueeze(RCNN_loss_cls, 0)
+            RCNN_loss_bbox = torch.unsqueeze(RCNN_loss_bbox, 0)
+
 
 
         cls_prob = cls_prob.view(batch_size, rois.size(1), -1)
