88d62743b871de65bf0b22cae921e08514b11ae4
Deep Chakraborty <deepc94@gmail.com>
2019-02-11 09:14:52 -0500

    changed training subset for training

--- faster-rcnn.pytorch/trainval_net.py
+++ faster-rcnn.pytorch/trainval_net.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
       args.imdbval_name = "vg_150-50-50_minival"
       args.set_cfgs = ['ANCHOR_SCALES', '[2, 4, 8, 16]', 'ANCHOR_RATIOS', '[0.5,1,2]', 'MAX_NUM_GT_BOXES', '50']#sclaes=[4,8,16,32]--default
   elif args.dataset == "kaist":
-      args.imdb_name = "train_subset_100"
+      args.imdb_name = "combined_train"
       args.imdbval_name = "train-all02"
       args.set_cfgs = ['ANCHOR_SCALES', '[0.05, 0.1, 0.25, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4]', 'ANCHOR_RATIOS', '[0.5, 1, 2]', 'MAX_NUM_GT_BOXES', '30']#scales=[4,8,16,32] ratios = [0.5, 1, 2]--default
   
